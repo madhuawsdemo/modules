7525cf2dbbc585d56279738ceda050e1b6adc7da
retheshnair <retheshnair@users.noreply.github.com>
2021-07-14 01:30:53 +0530

    Update variables.tf

--- kubernetes/namespace/variables.tf
+++ kubernetes/namespace/variables.tf
@@ -4,32 +4,13 @@ variable "namespace_names" {
 }
 
 
-variable "host" {
-  description = "K8S Host Name"
+variable "config_path" {
+  description = "Config Path"
   type        = string
 }
 
 
-variable "client_certificate" {
-  description = "K8S Cluster Client Certificate"
+variable "config_context" {
+  description = "config_context"
   type        = string
 }
-
-variable "client_key" {
-  description = "K8S Cluster Client Key"
-  type        = string
-}
-
-
-variable "cluster_ca_certificate" {
-  description = "K8S Cluster Client Certificate CA"
-  type        = string
-}
-
-/*
-variable "load_config_file" {
-  description = "K8S Cluster Config File Load"
-  type        = bool
-  default     = false
-}
-*/
